f37cfc87121f400b14c46a3e5ee0aa3d21f27a7d
avirupdutta <avirupdutta10@gmail.com>
2021-03-02 18:22:03 +0530

    new update

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,26 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send({
-    data: 'Hello From Express API',
+    data: 'Hello From Express API v2.0',
     status: res.status
   })
 });
+
+router.post('/authenticate', function (req, res, next) {
+  const email = req.body.email
+  const password = req.body.password
+  if (email === 'test@user.com') {
+    if (password === '123456') {
+      return res.send({
+        status: 'OK',
+        data: 'Authenticated!'
+      }).status(200)
+    }
+  }
+  return res.send({
+    status: 'UNAUTHORIZED',
+    data: 'Login Failed'
+  }).status(403)
+})
 
 module.exports = router;
